6e5541c9d3536ab35f30879b683c12baefc5f60e
Diego Lafuente <dlaf88@gmail.com>
2015-09-03 15:12:30 +0000

    adding rails production

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,9 +64,11 @@ class PostsController < ApplicationController
  protected
 
 def authenticate
-  authenticate_or_request_with_http_basic do |username, password|
-    username == ENV['HTTP_USER'] && password == ENV['HTTP_PASS']
-  end
+  if Rails.env == 'production'
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['HTTP_USER'] && password == ENV['HTTP_PASS']
+    end
+  end 
 end
   
   
